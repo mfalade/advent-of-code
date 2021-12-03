ca091af793cfe48c5bc1a761a4ef0124e270fe77
Mayowa Falade <mfalade@waveapps.com>
2021-12-03 13:16:20 -0500

    Create utility to create directory structure for new challenges

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # advent-of-code
+
+A collection of my solution to the 2021 Advent of Code challenge. 
+
+
+### lib
+- inputParser.ts: Formats the input, trims the input and removes empty characters
+- createChallengeDirectory.ts: Bootstraps a directory for a new challenge. The created directory is prefilled with an index.ts, challenge1.ts, challenge2.ts, input.txt and the necessary imports between all these are wired together. 
---
+++ lib/createChallengeDirectory.ts
@@ -0,0 +1,72 @@
+import fs from "fs";
+import path from "path";
+
+import { INPUT_FILE_NAME } from "../baseConfig";
+
+export interface ICreateChallengeDirectoryArgs {
+  challengeName: string;
+}
+
+const getChallengeDirectoryPath = (challengeName: string): string => {
+  return path.join(process.cwd(), "src", challengeName);
+};
+
+const getIndexFileContent = (challengeName: string): string =>
+  `import getParsedInput from "../../lib/inputParser";
+
+const CHALLENGE_DIR_NAME = "${challengeName}";
+
+const input: string[] = getParsedInput({
+  challengeDirName: CHALLENGE_DIR_NAME,
+});
+
+export default input;
+`;
+
+const getChallengeFileContent = (): string => {
+  return `import input from "./index";`;
+};
+
+const createChallengeDirectory = ({
+  challengeName,
+}: ICreateChallengeDirectoryArgs) => {
+  const directoryPath = getChallengeDirectoryPath(challengeName);
+
+  fs.mkdirSync(directoryPath);
+
+  const indexFile = path.join(directoryPath, "index.ts");
+  const inputFile = path.join(directoryPath, INPUT_FILE_NAME);
+  const challenge1File = path.join(directoryPath, "challenge1.ts");
+  const challenge2File = path.join(directoryPath, "challenge2.ts");
+
+  fs.writeFileSync(indexFile, getIndexFileContent(challengeName));
+  fs.writeFileSync(inputFile, "");
+  fs.writeFileSync(challenge1File, getChallengeFileContent());
+  fs.writeFileSync(challenge2File, getChallengeFileContent());
+
+  console.log(`Created a new challenge directory: [${challengeName}]`);
+};
+
+const execute = () => {
+  const challengeName = process.argv[2];
+
+  if (!challengeName) {
+    console.log(
+      "No challenge name was provided. Pass challenge name as `yarn mkdir someChallenge`"
+    );
+    process.exit();
+  }
+
+  const directoryPath = getChallengeDirectoryPath(challengeName);
+
+  if (fs.existsSync(directoryPath)) {
+    console.log(
+      `A challenge with the provided name [${challengeName}] already exists`
+    );
+    process.exit();
+  }
+
+  createChallengeDirectory({ challengeName });
+};
+
+execute();

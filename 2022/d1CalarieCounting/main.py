d9ef6eb843f401b5dadab62f83b782bb5ba45a0b
Mayowa Falade <mfalade@waveapps.com>
2022-12-01 14:17:28 -0500

    Minimal Python setup for day 1

---
+++ 2022/d1CalarieCounting/main.py
@@ -0,0 +1,24 @@
+import heapq
+import os
+
+
+def main():
+    with open(os.path.join(os.curdir, 'input1.txt'), 'r') as input_file:
+        content = input_file.read()
+        chunks = content.split('\n\n')
+
+        list_of_sums = []
+
+        for chunk in chunks:
+            cleaned = [int(item) for item in chunk.split('\n')]
+            sum_of_cleaned = sum(cleaned)
+            list_of_sums.append(sum_of_cleaned)
+
+        largest_3 = heapq.nlargest(3, list_of_sums)
+
+        print(largest_3)
+        print(sum(largest_3))
+
+
+if __name__ == "__main__":
+    main()

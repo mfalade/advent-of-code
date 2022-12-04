7134426c232379202afc1332c1a7c8c4e97d3413
Mayowa Falade <mfalade@waveapps.com>
2022-12-04 01:18:22 -0500

    Move custom printer to utils

--- 2022/utils.py
+++ 2022/utils.py
@@ -18,15 +18,21 @@ def read_input_file(directory_name, file_name="input.txt"):
         return input_file.read()
 
 
+def pretty_print(text):
+    # Not the standard pprint.
+    print("." * (len(text) + 1))
+    print(text)
+    print("." * (len(text) + 1))
+
+
 def bootstrap_directory(directory_name):
     os.mkdir(directory_name)
 
     open(f"{directory_name}/__init__.py", "w").close()
     open(f"{directory_name}/input.txt", "w").close()
-    
+
     with (
         open(f"{directory_name}/main.py", "w") as main_file,
         open("template.txt", "r") as template_file,
     ):
         main_file.write(template_file.read())
-
